0eb15f1ce70d6431953c53ec9fdd4a4381b17dab
Mahmoud <mahmoudelmizayin@gmail.com>
2026-07-17 22:53:44 +0300

    Question1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace A_Book_With_A_Title_And_Pages
+{
+    internal class Program
+    {
+        class BookInfo
+        {
+            public string Title { get; set; }
+            public int Pages { get; set; }
+        }
+        static void Main(string[] args)
+        {   
+           
+
+        Console.WriteLine("========== Question 1 ==========");
+
+            object bookObj = new BookInfo
+            {
+                Title = "C# Basics",
+                Pages = 250
+            };
+
+            Console.WriteLine(bookObj);
+        }
+    }
+}
